b44997cb26ce8610b65635186baeebe92c0db710
Taylor Howell <howelltaylor195@gmail.com>
2023-10-31 23:22:12 -0500

    removing pusher endpoint problem

--- src/pages/api/pusher.ts
+++ src/pages/api/pusher.ts
@@ -1,7 +1,7 @@
 import { type NextApiRequest, type NextApiResponse } from "next";
 
-import Pusher from "pusher";
-import { env } from "process";
+// import Pusher from "pusher";
+// import { env } from "process";
 
 const apiRoute = (req: NextApiRequest, res: NextApiResponse) => {
   return res.status(200).json({ message: "OK" });
@@ -11,42 +11,42 @@ const apiRoute = (req: NextApiRequest, res: NextApiResponse) => {
     return;
   }
 
-  const { socketId } = req.body as { socketId: string };
-  const { channelName } = req.body as { channelName: string };
-  const { userName } = req.body as { userName: string };
+  // const { socketId } = req.body as { socketId: string };
+  // const { channelName } = req.body as { channelName: string };
+  // const { userName } = req.body as { userName: string };
 
-  if (!socketId || !channelName || !userName) {
-    res.status(400).json({ message: "Missing required fields" });
-    return;
-  }
+  // if (!socketId || !channelName || !userName) {
+  //   res.status(400).json({ message: "Missing required fields" });
+  //   return;
+  // }
 
-  const appId = env.PUSHER_APP_ID;
-  const key = env.PUSHER_KEY;
-  const secret = env.PUSHER_SECRET;
-  const cluster = env.PUSHER_CLUSTER;
+  // const appId = env.PUSHER_APP_ID;
+  // const key = env.PUSHER_KEY;
+  // const secret = env.PUSHER_SECRET;
+  // const cluster = env.PUSHER_CLUSTER;
 
-  if (!appId || !key || !secret || !cluster) {
-    res
-      .status(500)
-      .json({ message: "Not configured", appId, key, secret, cluster });
-    return;
-  }
+  // if (!appId || !key || !secret || !cluster) {
+  //   res
+  //     .status(500)
+  //     .json({ message: "Not configured", appId, key, secret, cluster });
+  //   return;
+  // }
 
-  const pusher = new Pusher({
-    appId,
-    key,
-    secret,
-    cluster,
-    useTLS: true,
-  });
+  // const pusher = new Pusher({
+  //   appId,
+  //   key,
+  //   secret,
+  //   cluster,
+  //   useTLS: true,
+  // });
 
-  const authResponse = pusher.authorizeChannel(socketId, channelName, {
-    user_id: userName,
-  });
+  // const authResponse = pusher.authorizeChannel(socketId, channelName, {
+  //   user_id: userName,
+  // });
 
-  res
-    .status(200)
-    .json({ ...authResponse, channelData: JSON.stringify({ userName }) });
+  // res
+  //   .status(200)
+  //   .json({ ...authResponse, channelData: JSON.stringify({ userName }) });
 };
 
 export default apiRoute;
